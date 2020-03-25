5efe6955207739ae123f47394ce849e5d65b3c7c
RodrigoFreireFonseca <rodrigofreire12@gmail.com>
2020-03-25 09:07:10 -0300

    Criação do backend

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/casos', IncidentController.index);
+routes.post('/casos', IncidentController.create);
+routes.delete('/casos/:id', IncidentController.delete);
+
+module.exports = routes;
