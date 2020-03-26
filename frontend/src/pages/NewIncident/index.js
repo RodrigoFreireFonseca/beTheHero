8ec5e0398c95bcbfeadfd3a2aed2b306c34d60f6
RodrigoFreireFonseca <rodrigofreire12@gmail.com>
2020-03-25 23:21:46 -0300

    Fim do Frontend

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImage from '../../assets/logo.svg';
+
+export default function NewIncident() {
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImage} alt="Logo"/>
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um heroi para resolver isso.</p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041"/>
+            Página Inicial
+          </Link>
+        </section>
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Whatsapp" />
+          <input placeholder="Valor em reais" />
+          <button className="button" type="submit" >Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
